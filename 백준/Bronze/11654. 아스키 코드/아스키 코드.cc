2fc28b3190e93ef02bc2d345252b7aa174972f26
racheliee <146931361+racheliee@users.noreply.github.com>
2024-01-24 01:23:14 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <set>
+
+using namespace std;
+
+int main(){
+    // cin, cout 속도 향상
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    char n;
+    cin >> n;
+
+    cout << (int)n;
+
+    return 0;
+}
