8f967c70f478a9b5fc682fbb21800c21c2d97168
racheliee <146931361+racheliee@users.noreply.github.com>
2024-01-25 01:03:25 +0900

    [Silver IV] Title: 듣보잡, Time: 40 ms, Memory: 7508 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <list>
+
+using namespace std;
+
+bool comp(string a, string b){
+    return a < b;
+}
+
+int main(){
+    // cin, cout 속도 향상
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, m;
+    cin >> n >> m;
+
+    unordered_map<string, int> not_heard;
+    list<string> neither;
+
+    for(int i = 0; i < n; i++){
+        string name;
+        cin >> name;
+
+        not_heard[name] = 1;
+    }
+
+    int index = 0;
+    for(int i = 0; i < m; i++){
+        string name;
+        cin >> name;
+
+        if(not_heard.find(name) != not_heard.end()){
+            neither.push_back(name);
+            index++;
+        }
+    }
+
+    cout << index << "\n";
+    neither.sort();
+    for(auto i : neither){
+        cout << i << "\n";
+    }
+
+    return 0;
+}
