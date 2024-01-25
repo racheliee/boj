a266b2b1e1c6b21e2de099c6a0bcd03069c1b7ea
racheliee <146931361+racheliee@users.noreply.github.com>
2024-01-25 23:19:06 +0900

    [Bronze V] Title: Hello World, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <set>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cout << "Hello World!";
+    return 0;
+}
