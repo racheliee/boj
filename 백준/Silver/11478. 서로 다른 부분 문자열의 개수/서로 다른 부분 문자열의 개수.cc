0f5f9df0625923d630841d5708bcfa770bee7b3f
racheliee <146931361+racheliee@users.noreply.github.com>
2024-01-25 22:08:03 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 656 ms, Memory: 209852 KB -BaekjoonHub

---
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <unordered_set>
+
+using namespace std;
+
+int main(){
+    // cin, cout 속도 향상
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string str;
+    cin >> str;
+
+    unordered_set<string> substr;
+
+    int length = str.length();
+    for(int i = 1; i <= length; i++){ //i = length
+        for(int j = 0; j < length; j++){ //j = start index
+            //substring in c++ is string.substr(start index, length)
+            substr.insert(str.substr(j, i));
+        }
+    }
+
+    cout << substr.size();
+
+    return 0;
+}
