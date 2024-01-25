13a940b8e298827afcc9675e6f85ac20d1fd5c1c
racheliee <146931361+racheliee@users.noreply.github.com>
2024-01-25 23:20:32 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <set>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int x, y;
+    cin >> x >> y;
+
+    cout << x*y;
+    
+    return 0;
+}
