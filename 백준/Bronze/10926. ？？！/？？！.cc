af67ebd39449645521862c3a09ae4fa2002b49cc
racheliee <146931361+racheliee@users.noreply.github.com>
2024-01-25 23:25:46 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string str;
+    cin >> str;
+
+    cout << str << "??!";
+
+    return 0;
+}
