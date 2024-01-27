9cd50f3d2191037314c0b6ff8b528270a95407b8
racheliee <146931361+racheliee@users.noreply.github.com>
2024-01-28 03:18:03 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 108 ms, Memory: 13664 KB -BaekjoonHub

---
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+bool compare(const pair<string, int> a, const pair<string, int> b){
+    if(a.second != b.second){ //num of occurences are different
+        return a.second > b.second;
+    }else if(a.first.length() != b.first.length()){ //length is different
+        return a.first.length() > b.first.length();
+    }else{
+        return a.first < b.first;
+    }
+}
+
+int main(){
+    // cin, cout 속도 향상s
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, m; 
+    cin >> n >> m;
+
+    unordered_map<string, int> vocab_map;
+
+    string vocab;
+    while(n){
+        n--;
+        
+        cin >> vocab;
+
+        if(vocab.length() < m){
+            continue;
+        }else{
+            if(vocab_map.find(vocab) == vocab_map.end()){ //not found yet
+                vocab_map[vocab] = 1;
+            }else{ //vocab word exists
+                vocab_map[vocab]++;
+            }
+        }
+    }
+
+    vector<pair<string, int> > vocab_vector(vocab_map.begin(), vocab_map.end());
+    sort(vocab_vector.begin(), vocab_vector.end(), compare);
+
+    for(auto it = vocab_vector.begin(); it != vocab_vector.end(); ++it){
+        cout << it->first << "\n";
+    }
+    
+    return 0;
+}
