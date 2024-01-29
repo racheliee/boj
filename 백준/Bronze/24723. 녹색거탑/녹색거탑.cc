822e216ac9441d02d2ddb3111ee64b5f444dc59d
racheliee <146931361+racheliee@users.noreply.github.com>
2024-01-29 16:39:07 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    cout << pow(2, n);
+    return 0;
+}
