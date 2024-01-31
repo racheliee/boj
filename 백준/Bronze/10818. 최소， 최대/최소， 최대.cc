9efdbb7989cc0615a72f4104dca5704cca2814f7
racheliee <146931361+racheliee@users.noreply.github.com>
2024-01-31 22:36:00 +0900

    [Bronze III] Title: 최소, 최대, Time: 108 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
@@ -1,12 +1,11 @@
 #include <iostream>
-#include <algorithm>
 
 using namespace std; 
 
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
     int n;
     cin >> n;
@@ -14,11 +13,11 @@ int main(){
     int num;
     int min_v = 1000001;
     int max_v = -1000001;
-    while(n){
-        n--;
+    
+    for(size_t i = 0; i < n; i++){
         cin >> num;
-        min_v = min(num, min_v);
-        max_v = max(num, max_v);
+        if(num > max_v) max_v = num;
+        if(num < min_v) min_v = num;
     }
 
     cout << min_v << " " << max_v;
