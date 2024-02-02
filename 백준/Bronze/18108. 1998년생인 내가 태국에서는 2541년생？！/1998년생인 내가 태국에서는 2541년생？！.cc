73459a82212cb9dfa21376c1f71fb84ca7106b63
racheliee <146931361+racheliee@users.noreply.github.com>
2024-02-02 23:50:00 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <cmath>
+#include <deque>
+#include <iostream>
+#include <string.h>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    cout << n-543;
+}
