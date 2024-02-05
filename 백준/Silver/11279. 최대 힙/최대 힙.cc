c83f26e64afbd360513c086866e31dd9fbb248d3
racheliee <146931361+racheliee@users.noreply.github.com>
2024-02-05 21:54:06 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+
+using namespace std;
+
+int n;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+
+    priority_queue<int> pq;
+
+    int temp;
+    while (n--) {
+        cin >> temp;
+        if (temp)
+            pq.push(temp);
+        else {
+            if (pq.empty())
+                cout << 0 << endl;
+            else {
+                cout << pq.top() << endl;
+                pq.pop();
+            }
+        }
+    }
+
+    return 0;
+}
