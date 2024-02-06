047ee9364a4e2f0da7818626f1182f527f3d4816
racheliee <146931361+racheliee@users.noreply.github.com>
2024-02-06 13:01:18 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    long long a, b, c;
+    cin >> a >> b >> c;
+
+    cout << a + b + c;
+
+}
