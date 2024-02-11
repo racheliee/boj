9b2755b569362ed288a126d447917a3920a37b4a
racheliee <146931361+racheliee@users.noreply.github.com>
2024-02-11 21:56:40 +0900

    [Gold IV] Title: 부분합, Time: 8 ms, Memory: 3588 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+
+using namespace std;
+
+int n;
+long long s;
+vector<long long> arr = {
+    0,
+};
+vector<long long> sum = {
+    0,
+};
+
+void closestPair() {
+    cin >> n >> s;
+    arr.resize(n);
+    sum.resize(n + 1);
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    partial_sum(arr.begin(), arr.end(), sum.begin() + 1); //+1 so the first element can be 0
+
+    // for (int i = 0; i < n + 1; i++) {
+    //     cout << sum[i] << " ";
+    // } cout << endl;
+
+    int min_num = INT_MAX;
+
+    int i = 0;
+    int j = 1;
+    int series_sum;
+
+    while (i < j && j < n+1) {
+        series_sum = sum[j] - sum[i];
+        // cout << series_sum << " ";
+
+        if (series_sum >= s) {
+            if (j - i < min_num) {
+                min_num = j - i;
+            }
+            // cout << "min: " << min_num << " i: " << i << " j: " << j << endl;
+        }
+
+        // pointers need to move towards each other
+        if (series_sum < s) { // if the sum is larger than s, decrease j for a smaller sum
+            j++;
+        } else {
+            i++;
+        }
+    }
+
+    if(min_num == INT_MAX) cout << 0;
+    else cout << min_num;
+
+    return;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    closestPair();
+
+    return 0;
+}
