1be08b541f418913968e06676b7d75b455455df7
racheliee <146931361+racheliee@users.noreply.github.com>
2024-02-11 20:15:08 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+
+using namespace std;
+
+int res[9];
+bool used[9] = {false};
+int n, m;
+
+void backTrack(int index){
+    if(index == m){
+        for(int i = 0; i < m; i++){
+            cout << res[i] << " ";
+        }
+        cout << endl;
+        return;
+    }
+
+    for(int i = 1; i <=n; i++){
+        if(!used[i] && (index == 0 || res[index-1] < i)){ //not used (false)
+            res[index] = i;
+            used[i] = true;
+            backTrack(index+1);
+            used[i] = false;
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> m;
+
+    backTrack(0);
+
+    return 0;
+}
