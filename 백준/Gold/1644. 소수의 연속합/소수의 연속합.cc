ff48ba2f4bb42b10b24687532550ae59181eedf1
racheliee <146931361+racheliee@users.noreply.github.com>
2024-02-11 23:39:20 +0900

    [Gold III] Title: 소수의 연속합, Time: 52 ms, Memory: 23932 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
@@ -4,14 +4,13 @@
 using namespace std;
 
 int n;
-vector<int> arr;
 vector<long long> sum;
 
 void find_primes(){
     vector<int> primes(n+1, 1);
     primes[0] = 0; primes[1] = 0;
 
-    for(int i = 2; i <= n; i++){
+    for(int i = 2; i <= sqrt(n); i++){
         if(primes[i] == 0) continue;
         else{
             for(int j = i*2; j <= n; j += i){
@@ -23,7 +22,7 @@ void find_primes(){
     sum.push_back(0);
     for(int i = 2; i <= n; i++){
         if(primes[i]){
-            sum.push_back(sum.back() + i);
+            sum.push_back(sum.back() + i); //getting partial sum
         }
     }
 }
@@ -48,7 +47,7 @@ void closestPair() {
         }
 
         // pointers need to move towards each other
-        if (series_sum < n) { // if the sum is less than s, increase j for a larger sum
+        if (series_sum < n) { // if the sum is less than n, increase j for a larger sum
             j++;
         } else {
             i++;
