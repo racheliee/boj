38478edafe2b546bb614854233072e54c66429a0
racheliee <146931361+racheliee@users.noreply.github.com>
2024-02-15 00:57:56 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze V/2739. 구구단/구구단.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= 9; i++){
+        cout << n << " * " << i << " = " << n*i << endl;    
+    }
+}
