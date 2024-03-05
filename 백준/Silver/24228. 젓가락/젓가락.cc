1d49d6fc884c8b015e748f068c33acde3a3f2c32
racheliee <146931361+racheliee@users.noreply.github.com>
2024-03-05 14:36:30 +0900

    [Silver V] Title: 젓가락, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/24228. 젓가락/젓가락.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+
+using namespace std;
+
+long long int n, r;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> r;
+    cout << n + 2*r-1;
+
+    return 0;
+}
